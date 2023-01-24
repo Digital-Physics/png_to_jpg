25facde248c9c1f30bd6147ed39a43f54f155380
Jonathan Khanlian <jdkhanlian@gmail.com>
2023-01-23 20:05:51 -0500

    updates to deleting files and saving a backup first

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ from PIL import Image
 import os
 from pathlib import Path
 
-for i in range(-1, 3):
-    mypath = f"/Users/jonathankhanlian/PycharmProjects/web-game/img/transition/level_{i}/"
+# backup location
+save_path = "/Users/jonathankhanlian/Desktop/original_pngs_of_digital_physics_video_game/"
+
+for i in range(15):
+    mypath = f"/Users/jonathankhanlian/PycharmProjects/web-game/img/movie_png_seq{i}/"
 
     for file in os.listdir(mypath):
         if file.endswith(".png"):
             file_name = Path(mypath + file).stem
             img = Image.open(mypath + file)
             rgb_im = img.convert("RGB")
-            rgb_im.save(mypath + file_name + ".jpg")
+            rgb_im.save(mypath + file_name + ".jpg", quality=100)
+
+            # save it somewhere else just in case we want it again
+            img.save(save_path + file)
+
+            # delete .png after using it and (saving it somewhere else just in case)
+            os.remove(mypath + file)
